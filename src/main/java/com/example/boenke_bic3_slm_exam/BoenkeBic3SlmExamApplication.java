d85a39326f937d8a8f8a1f6b830638e149e59fc6
Thorina Boenke <thorina-boenke@web.de>
2022-12-21 18:02:58 +0100

    Initial commit

---
+++ src/main/java/com/example/boenke_bic3_slm_exam/BoenkeBic3SlmExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.boenke_bic3_slm_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoenkeBic3SlmExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoenkeBic3SlmExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boenke_bic3_slm_exam/BoenkeBic3SlmExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boenke_bic3_slm_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoenkeBic3SlmExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
